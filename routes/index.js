0344fc18a57ed339fcb73921be0c0adab9b45098
Asokumar Gurusamy <agurusamy@paypal.com>
2017-07-03 23:28:38 -0700

    adding dist directory"

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ export default function (router) {
 	router.get(['/*'], function (req, res) {
 		console.log("req.url: ", req.url);
 		// react engine has a bug so we need this conditional check
-		if (req.url === '/favicon.ico') {
+		if (req.url === '/favicon.ico' || req.url.includes(".css") || req.url.includes(".js")) {
 			return res.sendStatus(204);
 		}
 		return res.render(req.url);
